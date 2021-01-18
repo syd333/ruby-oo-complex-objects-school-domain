62375e00a3388c93fad5df47db1a3ad6e8789145
syd333 <sydmiranda@icloud.com>
2021-01-18 14:58:26 -0500

    commit tree 6c73ed21baa1279dee6fa5be31bfc6663bcdb693

--- lib/school.rb
+++ lib/school.rb
@@ -1,29 +1,23 @@
 # code here!
-
+require 'pry'
 class School
-    attr_accessor :roster 
 
-def initialize(school_name)
-@roster= {}
-end
+attr_accessor :name, :roster
 
-
-def add_student(student_name, grade)
-   if @roster[grade]
-    @roster[grade] << student_name
-   else 
-    @roster[grade] = [student_name]
-   end
-end
-
-def grade(grade)
-    @roster[grade]
-end
-
-def sort
-    @roster.each do |key, value|
-        value.sort!
+    def initialize(name)
+        @name = name 
+        @roster = {}
     end
-end
+
+    def add_student(student_name, grade)
+        roster[grade] = []
+        roster[grade] << student_name 
+    else 
+        roster[grade] = [student_name]
+    end
+    end
 
 end
+
+# school.add_student("Zach Morris", 9)
+# school.roster
